0164a335254bbe465c4cf1f736101ed265851388
antoine-2beco <antoinedb@RB-MA.local>
2023-09-27 20:46:43 +0200

    corr ex00

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -22,7 +22,7 @@ int	ft_strcmp(char *s1, char *s2)
 		if (s1[i] > s2[i])
 			return (1);
 		if (s1[i] < s2[i])
-			return (-1); 
+			return (-1);
 		i++;
 	}
 	if ((s1[i] == '\0') && (s2[i] == '\0'))
